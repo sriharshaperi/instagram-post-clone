c7fae64b4107011d716b291eb457ae5651c2751f
Sri Harsha Peri <sriharshaperi@gmail.com>
2020-11-02 13:20:50 +0530

    first commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,16 @@
+require('dotenv').config()
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCZTllvpEyGTjzAZhWUmHpaGokKFZEaqyU",
-    authDomain: "instagram-clone-react-1e429.firebaseapp.com",
-    databaseURL: "https://instagram-clone-react-1e429.firebaseio.com",
-    projectId: "instagram-clone-react-1e429",
-    storageBucket: "instagram-clone-react-1e429.appspot.com",
-    messagingSenderId: "338718017862",
-    appId: "1:338718017862:web:00048cb0ee69ae2e9b3d47",
-    measurementId: "G-JD08Y70QK5"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGE_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 })
 
 const db = firebaseApp.firestore();
